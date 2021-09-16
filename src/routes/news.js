301d1747431b9bdb3f77ca4eb28e033ee71bc1a7
DESKTOP-4OLJ2SJ\hie09 <hie0908@naver.com>
2021-09-16 10:31:31 +0900

    Pulling Data from API

---
+++ src/routes/news.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const newsRouter = express.Router()
+const axios = require('axios')
+
+newsRouter.get('', async(req, res) => {
+    try {
+        const newsAPI = await axios.get(`https://raddy.co.uk/wp-json/wp/v2/posts/`)
+        res.render('news', { articles : newsAPI.data })
+    } catch (err) {
+        if(err.response) {
+            console.log(err.response.data)
+            console.log(err.response.status)
+            console.log(err.response.headers)
+        } else if(err.request) {
+            console.log(err.request)
+        } else {
+            console.error('Error', err.message)
+        }
+    } 
+})
+
+module.exports = newsRouter
